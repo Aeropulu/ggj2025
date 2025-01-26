14e4d335d5a47237cb313832af4aaa8864d8de34
Vidu From Space <74648825+ViduFromSpace@users.noreply.github.com>
2025-01-26 14:44:31 +0100

    MAJ

--- Scripts/Autoload/Event_Manager.gd
+++ Scripts/Autoload/Event_Manager.gd
@@ -1,11 +1,6 @@
 extends Node
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+## Ajout de score
+signal _on_add_score(add : int)
+func Invoke_On_Add_Score(add : int):
+	_on_add_score.emit(add)
