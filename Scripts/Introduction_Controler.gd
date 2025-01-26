5809a7eefdfb8570313916eae825e096d60cf89d
Vidu From Space <74648825+ViduFromSpace@users.noreply.github.com>
2025-01-26 14:08:40 +0100

    skip button

--- Scripts/Introduction_Controler.gd
+++ Scripts/Introduction_Controler.gd
@@ -1,6 +1,7 @@
 extends Control
 
 @export var _bouton_play : Button
+@export var _skip_button : TextureButton
 @export var _animation_player : AnimationPlayer
 
 func _ready() -> void:
@@ -8,9 +9,13 @@ func _ready() -> void:
 
 func Connect_Signaux():
 	_bouton_play.pressed.connect(_On_Bouton_Pressed)
+	_skip_button.pressed.connect(_On_Bouton_Skip_Pressed)
 	
 func _On_Bouton_Pressed():
 	_animation_player.play("Introduction")
-	
+
+func _On_Bouton_Skip_Pressed():
+	_Go_To_Game_Scene()
+
 func _Go_To_Game_Scene():
 	get_tree().change_scene_to_file("res://Scenes/Game.tscn")
