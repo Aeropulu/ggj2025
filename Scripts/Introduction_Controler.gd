23c4518b04c0aa4b825f712c4702b4569949f3fc
Aeropulu <42248174+Aeropulu@users.noreply.github.com>
2025-01-26 18:48:52 +0100

    Added sounds

--- Scripts/Introduction_Controler.gd
+++ Scripts/Introduction_Controler.gd
@@ -20,6 +20,10 @@ func _On_Bouton_Skip_Pressed():
 
 func Play_Music():
 	AudioManager.Play(load("res://Assets/Audio/GGJ SOGAMES - MDC - PireRates Cinematic.mp3"),AudioManager.Bus.MUSIQUE)
+	AudioManager.Play(load("res://Assets/Audio/SFX/SoundBybrunoboselli__pirate-tavern.wav"), AudioManager.Bus.BRUITAGE, 2.0)
 
 func _Go_To_Game_Scene():
 	get_tree().change_scene_to_file("res://Scenes/Game.tscn")
+
+func _input(event: InputEvent) -> void:
+	if event is InputEventKey and
