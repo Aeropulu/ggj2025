1b9f3fbd5be1ae48c8a2d76765cf3e16360db754
Aeropulu <42248174+Aeropulu@users.noreply.github.com>
2025-01-26 11:26:17 +0100

    bubble color and animation

--- Scripts/bulle.gd
+++ Scripts/bulle.gd
@@ -6,17 +6,34 @@ var falling_bubbles_parent: Node2D
 @export var coussin_color: Color
 
 @export var color_id: int = 0
+
+@export var diamant_sprite: Node2D
+@export var bulle_sprite: Node2D
+
+@export var noise: Noise
+@export var has_noise_anim: bool
+var noise_amplitude:float = 0.1
+var noise_dir: Vector2 = Vector2.RIGHT.rotated(0.2)
+var noise_speed: float = 30
+
+var phase: float
+var time: float = 0.0
+var amplitude: float = 0.5
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	#var board = get_parent()
-	#if not board is Board:
-		#return
-	#print(board.local_to_map(position))
+	phase = randf_range(0, 2 * PI)
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	if is_instance_valid(diamant_sprite):
+		diamant_sprite.rotation = sin(time + phase) * amplitude
+	if is_instance_valid(bulle_sprite) and has_noise_anim and is_instance_valid(noise) :
+		var noise_input = global_position + noise_dir * time * noise_speed
+		var noise_value = noise.get_noise_2dv(noise_input)
+		bulle_sprite.scale = Vector2.ONE * (1 + noise_value * noise_amplitude)
+	time += delta
 	pass
 		
 func apply_anim() -> void:
