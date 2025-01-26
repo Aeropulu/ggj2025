efe489ef057022efe27a70362b4e08d5f7d3c76f
Aeropulu <42248174+Aeropulu@users.noreply.github.com>
2025-01-26 16:56:43 +0100

    added level name field

--- Scripts/board.gd
+++ Scripts/board.gd
@@ -5,6 +5,7 @@ var width: int = 8
 var height: int = 10
 var min_to_match: int = 3
 
+@export var level_name: String = ""
 @export var is_tutorial: bool = false
 @export var rewards: Array[PackedScene]
 @export var default_reward: PackedScene = preload("res://Scenes/Rewards/Reward_defeat.tscn")
