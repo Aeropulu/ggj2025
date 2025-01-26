8dc505f1c060d4019f0c43e627b11ac58219fd1e
Vidu From Space <74648825+ViduFromSpace@users.noreply.github.com>
2025-01-26 13:15:15 +0100

    On continue

---
+++ Scripts/Introduction_Controler.gd
@@ -0,0 +1,16 @@
+extends Control
+
+@export var _bouton_play : Button
+@export var _animation_player : AnimationPlayer
+
+func _ready() -> void:
+	Connect_Signaux()
+
+func Connect_Signaux():
+	_bouton_play.pressed.connect(_On_Bouton_Pressed)
+	
+func _On_Bouton_Pressed():
+	_animation_player.play("Introduction")
+	
+func _Go_To_Game_Scene():
+	get_tree().change_scene_to_file("res://Scenes/Game.tscn")
